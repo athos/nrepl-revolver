381297e9ad9b13372dae6a7e0dcdc814cb90c998
OHTA Shogo <athos0220@gmail.com>
2016-10-21 14:06:42 +0900

    Replace Docker client to docker-java

--- project.clj
+++ project.clj
@@ -4,5 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [k2nr/docker "0.0.3-SNAPSHOT" :exclusions [clj-http]]
-                 [clj-http "2.3.0"]])
+                 [com.github.docker-java/docker-java "3.0.6"]])
