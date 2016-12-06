d3f05679f515b9ee77675136b4efb65cdcfea659
OHTA Shogo <athos0220@gmail.com>
2016-12-06 22:38:06 +0900

    Bump Clojure version

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/athos/nrepl-revolver"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.github.docker-java/docker-java "3.0.6"]])
