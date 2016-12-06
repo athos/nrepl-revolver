0631cd2f634e47d9bdf0adf12bf2da511733a233
OHTA Shogo <athos0220@gmail.com>
2016-12-06 22:37:37 +0900

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject nrepl-revolver "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Docker-backed nrepl server aiming for secure execution and fast startup"
+  :url "https://github.com/athos/nrepl-revolver"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
